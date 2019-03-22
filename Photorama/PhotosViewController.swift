7a3d237f9d435458f526cd104499cfff61cd2ea0
SnIvan <ivan.sakhan@icloud.com>
2019-03-21 21:55:28 -0400

    assignment 4

--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -9,10 +9,13 @@
 import Foundation
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UICollectionViewDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = photoDataSource
+        collectionView.delegate = self
         
         store.fetchInterestingPhotos() {
             (photosResult) -> Void in
@@ -20,28 +23,37 @@ class PhotosViewController: UIViewController {
             switch photosResult {
             case let .success(photos):
                 print("Successfully found \(photos.count) photos.")
-                if let firstPhoto = photos.first {
-                    self.updateImageView(for: firstPhoto)
-                }
+                self.photoDataSource.photos = photos
             case let .failure(error):
                 print("Error fetching interesting photos: \(error)")
+                self.photoDataSource.photos.removeAll()
             }
+            self.collectionView.reloadSections(IndexSet(integer: 0))
         }
     }
     
-    @IBOutlet var imageView: UIImageView!
+    
     var store: PhotoStore!
+    @IBOutlet var collectionView: UICollectionView!
     
-    func updateImageView(for photo: Photo) {
-        store.fetchImage(for: photo) {
-            (imageResult) -> Void in
+    let photoDataSource = PhotoDataSource()
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        let photo = photoDataSource.photos[indexPath.row]
+        
+        store.fetchImage(for: photo) { (result) -> Void in
+            guard let photoIndex = self.photoDataSource.photos.index (of: photo),
+                case let .success(image) = result else {
+                    return
+            }
             
-            switch imageResult {
-            case let .success(image):
-                self.imageView.image = image
-            case let .failure(error):
-                print("Error downloading image: \(error)")
+            let photoIndexPath = IndexPath(item: photoIndex, section: 0)
+            
+            if let cell = self.collectionView.cellForItem(at: photoIndexPath) as? PhotoCollectionViewCell {
+                cell.update(with: image)
             }
         }
     }
+    
 }
